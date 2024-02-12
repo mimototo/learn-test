a2366032b948501c3475443244abef69dbaddc06
mimototo <mail@mimototo.jp>
2024-02-12 21:42:06 +0900

    feat: create prisma seed data

---
+++ lib/nextauth.ts
@@ -0,0 +1,85 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { DefaultSession, NextAuthOptions, getServerSession } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/lib/prisma/prisma";
+import bcrypt from "bcrypt";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+}
+
+export const authOptions: NextAuthOptions = {
+  // prismaを使うための設定
+  adapter: PrismaAdapter(prisma),
+
+  // 認証プロバイダーの設定
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.password) {
+          return null;
+        }
+
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          user.password,
+        );
+        if (passwordMatch) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+};
+
+export const getAuthSession = async () => {
+  return getServerSession(authOptions);
+};
---
+++ prisma/seed.ts
@@ -0,0 +1,69 @@
+import { PrismaClient, User } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { fakerJA as faker } from "@faker-js/faker";
+
+// https://www.prisma.io/docs/orm/prisma-migrate/workflows/seeding
+
+const prisma = new PrismaClient();
+
+function getRandomDate(daysAgo: number) {
+  const today = new Date();
+  const pastDate = new Date(
+    today.getTime() - Math.random() * daysAgo * 24 * 60 * 60 * 1000,
+  );
+  return pastDate;
+}
+
+async function main() {
+  const hashedPassword = await bcrypt.hash("password1234", 10);
+  const mainUserEmail = "main-user@example.com";
+  try {
+    await resetData();
+
+    await prisma.user.deleteMany();
+
+    const mainUser = await prisma.user.create({
+      data: {
+        name: "main-user",
+        email: mainUserEmail,
+        password: hashedPassword,
+      },
+    });
+    await createBlogs({ user: mainUser });
+
+    for (let i = 1; i <= 5; i++) {
+      const subUser = await prisma.user.create({
+        data: {
+          name: faker.person.fullName(),
+          email: faker.internet.email(),
+          password: hashedPassword,
+        },
+      });
+      await createBlogs({ user: subUser });
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await prisma.$disconnect;
+  }
+}
+
+async function createBlogs({ user }: { user: User }) {
+  for (let i = 1; i <= 10; i++) {
+    const date = getRandomDate(30);
+    await prisma.blog.create({
+      data: {
+        title: faker.lorem.lines(1),
+        content: faker.lorem.lines({ min: 3, max: 5 }),
+        userId: user.id,
+        createdAt: date,
+      },
+    });
+  }
+}
+
+async function resetData() {
+  await prisma.user.deleteMany();
+}
+
+main();
